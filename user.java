3d152dfaa1693de356a2e35230bf432a320d00e1
sanghoon12 <71174003+sanghoon12@users.noreply.github.com>
2020-12-18 22:01:05 +0900

    Add files via upload

---
+++ user.java
@@ -0,0 +1,18 @@
+public class user{
+	String name;
+	int year;
+	int month;
+	int day;
+	String secretNum;
+	
+	public void input(String name, int year, int month, int day) {
+		this.name = name;
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	
+	public void save() {
+		
+	}
+}
